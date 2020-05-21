38c0167fd73cd7d12f3e1d6d3fb3b7b44a34389c
Fabricio Castro <baptistafabricio87@gmail.com>
2020-05-21 16:44:24 -0300

    primeiros scripts

---
+++ comecando/meuprimeiroexemplo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# primeiroexemplo.sh
+
+echo
+echo 'Olá mamãe!'
+echo
+echo 'Agora executarei um simples "ls -l", veja: '
+echo
+ls -l
+
---
+++ comecando/procurasuid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# procurasuid.sh
+
+# script para procurar arquivos suid que
+# pertençam a determinado usuário.
+
+find / -user $1 -perm -4000 2> /dev/null
+
